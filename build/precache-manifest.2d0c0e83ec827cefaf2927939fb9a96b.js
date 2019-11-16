9c158c23ad8f93af0544439867bdf4c1ab1d1f10
amazingshellyyy <amazingshellyyy@gmail.com>
2019-11-16 15:30:04 -0800

    robofriends

---
+++ build/precache-manifest.2d0c0e83ec827cefaf2927939fb9a96b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "206b1c6acb88db17ecef488864827b06",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d6fe50c24049df5ec882",
+    "url": "/static/css/2.299a5230.chunk.css"
+  },
+  {
+    "revision": "3971d481ee16b9d79d7b",
+    "url": "/static/css/main.eabfd85f.chunk.css"
+  },
+  {
+    "revision": "d6fe50c24049df5ec882",
+    "url": "/static/js/2.591f2f3b.chunk.js"
+  },
+  {
+    "revision": "3971d481ee16b9d79d7b",
+    "url": "/static/js/main.7a9b5649.chunk.js"
+  },
+  {
+    "revision": "9bf820bd41e6d656f8db",
+    "url": "/static/js/runtime-main.f59df13c.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
