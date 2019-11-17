10852beb13ee841d89d1bbaefa72e41a2cd833bd
amazingshellyyy <amazingshellyyy@gmail.com>
2019-11-16 16:14:37 -0800

    Updates

---
+++ precache-manifest.53f0bfce8d166a638605a9e2da5869a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f6c14af376d194291be592114b6b3b3",
+    "url": "/RoboFriends/index.html"
+  },
+  {
+    "revision": "d6fe50c24049df5ec882",
+    "url": "/RoboFriends/static/css/2.299a5230.chunk.css"
+  },
+  {
+    "revision": "2bed9ab2949edbd586c4",
+    "url": "/RoboFriends/static/css/main.e4a23d9c.chunk.css"
+  },
+  {
+    "revision": "d6fe50c24049df5ec882",
+    "url": "/RoboFriends/static/js/2.591f2f3b.chunk.js"
+  },
+  {
+    "revision": "2bed9ab2949edbd586c4",
+    "url": "/RoboFriends/static/js/main.6cf1e68a.chunk.js"
+  },
+  {
+    "revision": "6239f40d0ef81a2fd1e0",
+    "url": "/RoboFriends/static/js/runtime-main.6d5bc549.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RoboFriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
